f3973787953bb13129ed2850980b2bcd8d66fff3
bharanidharan1707 <bharanidharan.p@infodynamic.in>
2019-12-12 18:23:46 +0530

    EveningChanges

---
+++ Ba_Jet_App/Ba_Jet_App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ba_Jet_App.MvcApplication" Language="C#" %>
---
+++ Ba_Jet_App/Ba_Jet_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ba_Jet_App.Startup))]
+namespace Ba_Jet_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
